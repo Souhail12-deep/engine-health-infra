039c64d9036e4a7cf976390c8446385d43dc5e00
Souhail12-deep <khadraouisouhail1@gmail.com>
2026-04-02 10:44:51 +0100

    Revert: Restore working infra pipeline (single job version)

--- terraform/dev/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "engine-health-terraform-state"
-    key            = "dev/terraform.tfstate"
-    region         = "eu-north-1"
-    encrypt        = true
-  }
-}
